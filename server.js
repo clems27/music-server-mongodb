7892a4cd434e4f5a85046b30ab989b9d4cbb4d9c
Glitch (hello-express) <none>
2019-06-01 20:50:29 +0000

    🏋😻 Checkpoint ./server.js:4519493/131

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express()
 const uri = process.env.DATABASE_URI
 
 app.get('/', function(request, response) {
-  mongodb.MongoClient.connect(uri, function(error, client) {
+  const client = new mongodb.MongoClient(uri)
+
+  client.connect(function() {
     response.send('Hello, world!')
+    client.close()
   })
 })
 
