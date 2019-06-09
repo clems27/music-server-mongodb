7991c522cb292db439ac92742d7e713ba8a20e2f
Glitch (hello-express) <none>
2019-06-09 15:48:40 +0000

    👭🌽 Checkpoint ./server.js:4411779/1380

--- server.js
+++ server.js
@@ -24,13 +24,20 @@ app.get('/tracks/search', function(request, response) {
   client.connect(function() {
     const db = client.db("music")
     const tracksCollection = db.collection("tracks")
-    const seachObject ={
-      artist: request.query.artist,
-      title: request.query.title,
-      album: request.query.album,
-      year: parseInt(request.query.year)
+    const searchObject = {}
+    if(request.query.artist){
+       searchObject.artist=request.query.artist
     }
-    tracksCollection.find(seachObject).toArray(function(error, tracks){
+    if(request.query.album){
+       searchObject.artist=request.query.album
+    }
+    // {
+    //   artist: request.query.artist,
+    //   title: request.query.title,
+    //   album: request.query.album,
+    //   year: parseInt(request.query.year)
+    // }
+    tracksCollection.find(searchObject).toArray(function(error, tracks){
       response.json(error || tracks)
     client.close()
     })
