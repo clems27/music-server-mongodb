769f9e44198878f107aaca204eb2ac9bf02ea610
Glitch (hello-express) <none>
2019-06-12 11:24:03 +0000

    ⛽️🌵 Checkpoint ./server.js:4411779/297

--- server.js
+++ server.js
@@ -52,14 +52,14 @@ app.get('/tracks/:id', function(request, response) {
     const db = client.db('music')
     const tracksCollection = db.collection('tracks')
 
-    const string = '5cf2eb7d1c9d4400006fca92'
+    const string = tracksCollection._id
     const id = new mongodb.ObjectID(string)
-    const searchObject = { _id: id }
+    const searchObject = { }
 
     tracksCollection.findOne(searchObject, function(error, tracks) {
       
       console.log(error)
-      console.log(tracks)
+      console.log(searchObject )
       response.send(error || tracks)
       client.close()
     })
