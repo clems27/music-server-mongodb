01a84f5c6864da31a37cda15f74b7fe44ea41deb
Glitch (hello-express) <none>
2019-06-13 12:50:16 +0000

    👲🎒 Checkpoint ./server.js:4411779/373

--- server.js
+++ server.js
@@ -5,18 +5,6 @@ const app = express()
 
 const uri = process.env.DATABASE_URI
  
-// app.get('/', function(request, response) {
-//   const client = new mongodb.MongoClient(uri)
-
-//   client.connect(function() {
-//     const db = client.db("music")
-//     const tracksCollection = db.collection("tracks")
-//     tracksCollection.find().toArray(function(error, tracks){
-//       response.json(error || tracks)
-//     client.close()
-//     })
-//   })
-// })
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
@@ -33,6 +21,7 @@ app.get('/tracks', function(request, response) {
     })
   })
 })
+
 app.get('/tracks/new', function(request, response) {
   const client = new mongodb.MongoClient(uri)
 
